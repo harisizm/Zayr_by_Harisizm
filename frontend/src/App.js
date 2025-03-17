f6b3a2f599011d6f1d1550351a3d9523ac3902ff
harisizm <120340766+harisizm@users.noreply.github.com>
2025-03-18 01:39:15 +0500

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 function App() {
   return (
     <div>
+      <BrowserRouter>
      <Navbar/>
+      <Routes>
+        <Route path='/' element={<shop/>} />
+        <Route path='/men' element={<shopCategory category="men"/>} />
+        <Route path='/women' element={<shopCategory category="women"/>} />
+        <Route path='/kids' element={<shopCategory category="kids"/>} />
+        <Route path="/product" element={<product/>}>
+        <Route path=':productID' element={<product/>}/>
+
+        </Route>
+        <Route path='/cart' element={<cart/>}/>
+        <Route path='/login' element={<loginSignup/>}/>
+
+        </Routes>
+
+     </BrowserRouter>
+
     </div>
   );
 }
