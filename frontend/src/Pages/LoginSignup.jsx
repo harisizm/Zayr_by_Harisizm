3d3b12a8d5fdbad55959ff2461e75ab6889e2229
harisizm <120340766+harisizm@users.noreply.github.com>
2025-04-17 00:52:42 +0500

    login page design individial product pages setup

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import './CSS/LoginSignup.css'
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='login-signup'>
+      <div className="login-signup-container">
+          <h1>Sign Up</h1>
+          <div className="loginsignup-fields">
+            <input type="text" placeholder='Your Name' />
+            <input type="email" placeholder='Email Address' />
+            <input type="password" placeholder='Password' />  
+          </div>
+        <button>Continue</button>
+        <p className="login-signup-login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="login-signup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing I agree to the terms of use and privacy policy.</p>
+        </div>
+      </div>
     </div>
   )
 }
